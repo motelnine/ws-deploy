6f5891e3e3808596dc7ccdbb752dcff24684dafa
Matthew Smailey <them4tthew@gmail.com>
2024-01-26 14:12:49 -0700

    inetutils

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ yn "general dependencies"
 read GENERAL
 if [ $GENERAL == "y" ]
 then
-	sudo pacman -S ntfs-3g ffmpegthumbnailer gst-libav gst-plugins-base gst-plugins-good network-manager-applet dnsutils
+	sudo pacman -S ntfs-3g ffmpegthumbnailer gst-libav gst-plugins-base gst-plugins-good network-manager-applet dnsutils inetutils
 fi
 
 yn "firefox"
